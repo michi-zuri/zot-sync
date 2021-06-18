d8a5d06c2dd2b3c5872d14e07d4d022c80fcadad
M Killian <rad@killian.email>
2021-06-18 16:14:29 +0200

    v0.3.2

--- setup.py
+++ setup.py
@@ -1,22 +1,34 @@
 import setuptools
 
+required = [
+    'Click',
+    'SQLAlchemy',
+    'pyzotero',
+]
+
+# Please choose what works for you,
+# choose psycopg2 source distribution if it works on your system (recommended) or
+# opt for psycopg2-binary if you require a quick fix without guarantees (convenient).
+# example: pip install zot-sync[recommended]
+psycopg2 = {
+    'recommended': ['psycopg2'],
+    'alternative': ['psycopg2-binary']
+}
+
 setuptools.setup(
     name='zot-sync',
-    version='0.3.1',
+    version='0.3.2',
     description='Command-line interface to sync Zotero libraries to a local PostgreSQL database',
     author='Michael Paul Killian',
     author_email='rad@killian.email',
     url='https://github.com/michi-zuri/zot-sync',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Click',
-        'SQLAlchemy',
-        'psycopg2-binary',
-        'pyzotero',
-    ],
-    entry_points='''
-        [console_scripts]
-        zot-sync=src.sync:cli
-    ''',
+    install_requires= required,
+    extras_require= psycopg2,
+    entry_points={
+        'console_scripts': [
+            'zot-sync=zot_sync.cli:run',
+        ],
+    },
 )
